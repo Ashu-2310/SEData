f4e3e9a963dce30fd6ff33ea389249222e8eb525
Ashu2310 <ashutoshpande35@gmail.com>
2025-05-23 17:13:57 +0530

    add validation dependency

--- src/main/java/com/SB/SEData/model/SoftwareEngineer.java
+++ src/main/java/com/SB/SEData/model/SoftwareEngineer.java
@@ -1,4 +1,17 @@
 package com.SB.SEData.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class SoftwareEngineer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
+    private String name;
+    private String techStack;
+    private String department;
 }
