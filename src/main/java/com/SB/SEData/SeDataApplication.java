d71970a132a73762ccf8a9a52f591d6370ce491f
Ashu2310 <ashutoshpande35@gmail.com>
2025-05-23 17:00:10 +0530

    initial commit

---
+++ src/main/java/com/SB/SEData/SeDataApplication.java
@@ -0,0 +1,13 @@
+package com.SB.SEData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SB/SEData/SeDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SB.SEData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
